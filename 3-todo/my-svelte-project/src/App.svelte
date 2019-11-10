5f20fd2dec1c11a67575bf5b3d7b6824e38753f9
bernat <elbernat@gmail.com>
2019-11-10 18:25:28 -0500

    update

--- 3-todo/my-svelte-project/src/App.svelte
+++ 3-todo/my-svelte-project/src/App.svelte
@@ -1,126 +1,104 @@
 
 <!-- JS -->
 <script>
-    import Question from './Question.svelte';
-    import Answer from './Answer.svelte';
-    import Points from './Points.svelte';
 
-	let quizIndex = 0;
-    const quiz = [
-        {
-            question : "Whether God Exist or not?",
-            correctAnswer : 0,
-            answers : [
-                'Yes',
-                'No'
-            ]
-        },
-        {
-            question : "What is God?",
-            correctAnswer : 0,
-            answers : [
-                '?',
-                'A'
-            ]
-        },
-        {
-            question : "Of What is god?",
-            correctAnswer : 1,
-            answers : [
-                'All',
-                'Nothing'
-            ]
-        },
-        {
-            question : "Why is God?",
-            correctAnswer : 0,
-            answers : [
-                'No idea',
-                'Look inside'
-            ]
-        },
-        {
-            question : "How much is God?",
-            correctAnswer : 1,
-            answers : [
-                'Finite',
-                'Infinite'
-            ]
-        },
-        {
-            question : "Of what kind is God?",
-            correctAnswer : 0,
-            answers : [
-                'Unique',
-                'Vulgar'
-            ]
-        },
-        {
-            question : "When is God?",
-            correctAnswer : 0,
-            answers : [
-                'Now',
-                'Always'
-            ]
-        },
-        {
-            question : "Where is God?",
-            correctAnswer : 1,
-            answers : [
-                'Here',
-                'Everywhere'
-            ]
-        },
-        {
-            question : "How and with who is God?",
-            correctAnswer : 1,
-            answers : [
-                'Always',
-                'All the time'
-            ]
-        }
-	];
+    import Navbar from './Navbar.svelte'
 
-	let points = 0;
-	
-	function checkAnswerHandler() {
-		const isCorrect =  quiz[quizIndex].answers.indexOf(answerText) === quiz[quizIndex].correctAnswer;
+    let name = "EOSIO";
+    let symbol = "EOS";
+    let amount = 1000;
+    let showControls = false;
 
-		if ( isCorrect ) {
-			points += 1;
-			quiz[quizIndex].question = 'Correct!';
-		} else {
-			quiz[quizIndex].question = 'wrong';
-		}
-		setTimeout( function() {
-			quizIndex += 1;
-		}, 3000)
+    const addToken = ()=> amount+=1;
+    const removeToken = ()=> amount-=1;
+    const toggleControls = ()=> showControls =! showControls;
 
-	}
 
 </script>
 
 <!-- CSS -->
 <style >
-	/* .App {
-		
-	}
+    .symbol {
+        font-size: 1.5rem;
+        color: lightgray;
+    }
 
-	.Answers {
+    h1 {
+        letter-spacing: 8px
+    }
 
-	} */
+    h3 {
+        color: lightgray;
+    }
+
+    .container {
+	    max-width: 800px;
+	    margin: auto;
+	    overflow: hidden;
+	    padding: 0 2rem;
+    }
+
+    .card {
+	    padding: 1rem;
+	    border: #ccc 1px dotted;
+	    margin: 0.7rem 0;
+    }
+
+    .btn-light {
+        width: 20%;
+        border-radius: 4px;
+        color: black;
+        background-color: white;
+    }
+
+    .btn-dark {
+        width: 20%;
+        border-radius: 4px;
+        border-style: solid black;
+        border-width: 1px;
+        border-color: black;
+        color:white;
+        background-color: black;
+    }
+
+    .btn-small {
+        margin: 10px 10px;
+
+        font-size: 10px;
+        width: 20px;
+        border-radius: 4px;
+        border-style: solid;
+        border-width: 1px;
+        border-color: lightgray;
+        color:black;
+        background-color: white;
+    }
+
+    input {
+        width : 50%;
+    }
+
 
 </style>
 
 <!-- HTML -->
-<div class="App">
-	<Points points = { points }/>
-	<Question questionText = { quiz[quizIndex].question }/>
-	<div class="Answers">
-		<Answer answerText = { quiz[quizIndex].answers[0] checkAnswerHandler = "checkAnswerHandler" }/>
-		<Answer answerText = { quiz[quizIndex].answers[1] checkAnswerHandler = "checkAnswerHandler" }/>
-	</div>
+<Navbar/>
+<div class="container">
+    <div class="card">
+        <h2>{ name } | <span class="symbol">{ symbol }</span>
+        <button on:click = {toggleControls} class="btn-small">{#if showControls} - { :else } + { /if }</button></h2>
 
+        {#if showControls}
+            <button class="btn-light" on:click={addToken}> + </button>
+            <button class="btn-dark" on:click={removeToken}> - </button>
+        {:else}
+            <h3> No Controls </h3>
+        {/if}
+
+        <h1>{ amount }</h1>
+
+        <input type="number" bind:value = { amount }>
+    </div>
 </div>
 
 
